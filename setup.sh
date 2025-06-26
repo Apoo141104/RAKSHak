e9273ffb1bae166648badcf94ef8ba8d0f0df722
Devesh Attri <deveshattri16@gmail.com>
2025-06-27 00:37:37 +0530

    setup_update_commit

--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-python -m spacy download en_core_web_lg
+# The following line is commented out because en_core_web_lg is not used by the application,
+# and it can cause permission issues during deployment on Streamlit Cloud.
+# python -m spacy download en_core_web_lg
